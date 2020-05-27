c57aee2c799503e5db7fa86bccce87c5a15ba5ea
Felipe Sobreira <felipesobreira.c@hotmail.com>
2020-05-27 01:36:05 -0300

    Added search using input value

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const users = getUsers();
 
+const startEvents = () => {
+  let searchUserInput = document.querySelector('#userSearchInput');
+  searchUserInput.addEventListener('keyup', searchUser);
+};
+
 async function getUsers() {
   const users = await fetch(
     'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
@@ -8,7 +13,19 @@ async function getUsers() {
 }
 
 async function searchUser() {
-  users.then(console.log);
+  let searchUserInput = document.querySelector('#userSearchInput');
+  let inputValue = searchUserInput.value;
+  users.then((object) => {
+    results = object.results.filter((object) => {
+      return object.name.first.includes(inputValue);
+    });
+    showSearchResults(results);
+  });
 }
 
-searchUser();
+function showSearchResults(searchResults) {
+  console.log(searchResults);
+  // TODO
+}
+
+startEvents();
